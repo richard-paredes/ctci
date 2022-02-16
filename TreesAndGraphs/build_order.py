c11b6e91cc13cf9baac1d308f684d45bf1077c5a
Richard Paredes <richard.o.paredes@gmail.com>
2022-02-15 22:29:06 -0600

    temporary solution

--- TreesAndGraphs/build_order.py
+++ TreesAndGraphs/build_order.py
@@ -5,12 +5,31 @@ All of a project's dependencies must be built before the project is. Find a
 builder order that will allow the projects to be built. If there is no
 valid build order, return an error
 '''
-def solution(n):
+def build_dependencies(projects, dependencies):
+    dependency_graph = {}
+    for p in projects:
+        project_dependencies = [i for (i, j) in filter(lambda x: x[1] == p, dependencies)]
+        dependency_graph[p] = project_dependencies
+
+def solution(projects, dependencies):
+    dependency_graph = build_dependencies(projects, dependencies)
+    build_status = { p: False for p in projects }
+
+    dependency_list = [
+        (project, dependency_graph[project])
+        for project in dependency_graph
+    ]
+    print(dependency_list)
+    dependency_list.sort(key=lambda pd: len(pd[1]))
+    print(dependency_list)
     return False
 
 def main():
-    for test_case in []:
-        print(solution(test_case))
+    for test_case in [
+        (['A','B'], []),
+        (['A','B'], [('A', 'B')])
+    ]:
+        print(solution(test_case[0], test_case[1]))
 
 if __name__ == "__main__":
     main()
