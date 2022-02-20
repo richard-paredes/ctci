d26ea3c53a1639adba6a83116c05129ba89e9a67
Richard Paredes <richard.o.paredes@gmail.com>
2022-02-20 14:49:42 -0600

    check subtree

--- TreesAndGraphs/check_subtree.py
+++ TreesAndGraphs/check_subtree.py
@@ -5,8 +5,35 @@ A tree T2 is a subtree of T1 if there exists a node n in T1 such that
 the subtree of n is identical to T2. That is, if you cut off the
 tree at node n, the two trees would be identical
 '''
-def solution(n):
-    return False
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def are_equal(main, sub):
+    if main is None and sub is None:
+        return True
+    if main is None or sub is None:
+        return False
+    return main.value == sub.value and
+        are_equal(main.left, sub.left) and
+        are_equal(main.right, sub.right)
+
+def is_subtree(main_root, subtree_root):
+    if subtree_root is None:
+        return True
+    if main_root is None:
+        return False
+
+    if are_equal(main_root, subtree_root):
+        return True
+
+    return is_subtree(main_root.left, subtree_root) or
+        is_subtree(main_root.right, subtree_root)
+
+def solution(t, s):
+    return is_subtree(t, s)
 
 def main():
     for test_case in []:
